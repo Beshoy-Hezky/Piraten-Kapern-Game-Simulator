03c966db8933ff01ab53c3e6fc567ffe8d6d138e
Beshoy-Hezky <hezkyb@mcmaster.ca>
2023-09-05 15:33:59 -0400

    Fix small issue in main

--- src/main/java/PiratenKarpen.java
+++ src/main/java/PiratenKarpen.java
@@ -4,6 +4,7 @@ import pk.Game;
 public class PiratenKarpen {
 
     public static void main(String[] args) {
+        int num_of_games = 45;
         System.out.println("Welcome to Piraten Karpen Simulator!");
         Player first_player;
         Player second_player;
@@ -15,7 +16,7 @@ public class PiratenKarpen {
             second_player = new Player("random");
         }
         if(System.getProperties().containsKey("TRACE")){                                     // check if you want to run it in trace mode
-            for(int i = 0; i < 42; i++) {
+            for(int i = 0; i < num_of_games; i++) {
                 Game.totrace = true;
                 Game.playGame(first_player, second_player);
             }
@@ -26,10 +27,10 @@ public class PiratenKarpen {
                 Game.playGame(first_player, second_player);
             }
         }
-        float playerA_wins = (float)(first_player.getWins()/42.0)*100;      // calculate percentage wins
-        float playerB_wins = (float)(second_player.getWins()/42.0)*100;
-        System.out.printf("Player 1 wins: %.2f%% \n", playerA_wins);         // display percentage wins
-        System.out.printf("Player 2 wins: %.2f%% \n", playerB_wins);
+        float playerA_wins = (float)(first_player.getWins()/(num_of_games*1.0) )*100;      // calculate percentage wins
+        float playerB_wins = (float)(second_player.getWins()/(num_of_games*1.0) )*100;
+        System.out.printf("Player 1 wins: %.2f%% games \n", playerA_wins);         // display percentage wins
+        System.out.printf("Player 2 wins: %.2f%% games \n", playerB_wins);
         System.out.println("If the % dont add up the rest are ties");
         System.out.println("--------------END--------------");
 
